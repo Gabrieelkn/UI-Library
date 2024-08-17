a58902db46dfc4e2a9e685f50232790b384d1a8a
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-08-17 18:02:04 +0300

    update lock icon

--- app/_components/componentWrapper.jsx
+++ app/_components/componentWrapper.jsx
@@ -8,7 +8,6 @@ import Link from "next/link";
 
 export default function ComponentWrapper({ component, code, usage, title }) {
   const { user } = useUser();
-  console.log(user);
 
   return (
     <>
@@ -17,17 +16,18 @@ export default function ComponentWrapper({ component, code, usage, title }) {
       <Tabs defaultValue="component" className="m-auto">
         <TabsList className="mb-4">
           <TabsTrigger value="component">Component</TabsTrigger>
-          {user && user.email === "gabi8878@gmail.com" ? (
+          {!user || (user && user.email !== "ggabi8878@gmail.com") ? (
             <Link
               href="/all-access"
-              className="inline-flex w-6/12 md:w-[45%] mb-5 items-center justify-center whitespace-nowrap px-3 py-3 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:border-b-[1px] border-slate-700"
+              className="hover:cursor-pointer inline-flex w-6/12 md:w-[45%] mb-5 items-center justify-center whitespace-nowrap px-3 py-3 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:border-b-[1px] border-slate-700"
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width={25}
+                fill="currentColor"
                 height={25}
                 viewBox="0 0 50 50"
-                enable-background="new 0 0 50 50"
+                enableBackground="new 0 0 50 50"
               >
                 <path d="M34 23h-2v-4c0-3.9-3.1-7-7-7s-7 3.1-7 7v4h-2v-4c0-5 4-9 9-9s9 4 9 9v4z" />
                 <path d="M33 40H17c-1.7 0-3-1.3-3-3V25c0-1.7 1.3-3 3-3h16c1.7 0 3 1.3 3 3v12c0 1.7-1.3 3-3 3zM17 24c-.6 0-1 .4-1 1v12c0 .6.4 1 1 1h16c.6 0 1-.4 1-1V25c0-.6-.4-1-1-1H17z" />
