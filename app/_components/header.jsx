39a30119cdbfec572038a9e459677df8ea683966
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-08-17 18:02:34 +0300

    update styles

--- app/_components/header.jsx
+++ app/_components/header.jsx
@@ -15,10 +15,8 @@ export default function Header() {
   useEffect(() => {
     async function fetchUser() {
       const { data, error } = await supabase.auth.getUser();
-      console.log(data);
 
       if (data) setUser(data.user);
-      if (error) console.error("Error fetching user:", error);
     }
     if (!user) {
       fetchUser();
@@ -26,19 +24,25 @@ export default function Header() {
   }, [user, setUser]);
 
   return (
-    <header className="relative h-[10vh]">
-      <div className="container h-full flex justify-between items-center">
-        <Link className="hidden lg:block" href="/">
+    <header className="relative h-[20vh] md:h-[10vh]">
+      <div className="container flex-wrap md:flex-nowrap h-full flex justify-between items-center">
+        <Link
+          className="flex h-1/2 items-center justify-center w-[100px] h-[100px]" // Container styles
+          href="/"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            width="80"
-            height="80"
+            width="80" // Adjust width as needed
+            height="80" // Adjust height as needed
+            fill="green"
             viewBox="0 0 300 80"
+            className="text-center"
           >
             <text
-              y="55"
-              fontamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
-              fontSize="50"
+              x="0"
+              y="70"
+              fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
+              fontSize="80"
               className="fill-mainColor"
               fontWeight="bold"
             >
@@ -46,10 +50,10 @@ export default function Header() {
             </text>
 
             <text
-              x="90"
-              y="55"
+              x="150"
+              y="70"
               fontFamily="'Segoe UI', Tahoma, Geneva, Verdana, sans-serif"
-              fontSize="50"
+              fontSize="80"
               className="fill-current dark:text-white"
               fontWeight="bold"
             >
@@ -57,10 +61,11 @@ export default function Header() {
             </text>
           </svg>
         </Link>
+
         <SearchBar />
         <Navbar />
-        <div className="flex">
-          {user && <UserIcon />}
+        <div className="flex gap-3">
+          <UserIcon user={user} />
           <DarkModeToggle />
         </div>
       </div>
