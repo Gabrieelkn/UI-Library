10293a101c27c22bb660d71d4359d71168be7558
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-08-17 18:00:12 +0300

    update

--- shop/user/user.jsx
+++ shop/user/user.jsx
@@ -1,9 +1,8 @@
 export default function User() {
   return (
-    <div className="w-full  h-96 grid place-items-center">
-      <div className="flex items-start p-4 w-60 dark:bg-white bg-slate-500 rounded-lg shadow-md max-w-sm">
-        <div className="rounded-full dark:bg-gray-700 bg-background w-[50px] h-[50px] p-2 grid place-items-center">
-          {" "}
+    <div className="w-full h-96 grid place-items-center">
+      <div className="flex items-center p-4 w-64 bg-white dark:bg-gray-800 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300 ease-in-out">
+        <div className="rounded-full bg-gray-200 dark:bg-gray-700 w-12 h-12 p-2 flex items-center justify-center">
           <svg
             width="30px"
             height="30px"
@@ -13,16 +12,18 @@ export default function User() {
           >
             <path
               d="M5 21C5 17.134 8.13401 14 12 14C15.866 14 19 17.134 19 21M16 7C16 9.20914 14.2091 11 12 11C9.79086 11 8 9.20914 8 7C8 4.79086 9.79086 3 12 3C14.2091 3 16 4.79086 16 7Z"
-              stroke="#000000"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             />
           </svg>
         </div>
-        <div className="ml-4 text-textColor">
+        <div className="ml-4 text-gray-800 dark:text-gray-200">
           <span className="block text-lg font-semibold">Joe</span>
-          <span className="block text-sm">@joedoe</span>
+          <span className="block text-sm text-gray-600 dark:text-gray-400">
+            @joedoe
+          </span>
         </div>
       </div>
     </div>
