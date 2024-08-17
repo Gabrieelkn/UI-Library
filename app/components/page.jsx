1fe88887693d5f77bb5503228897f525760bb98a
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-08-17 18:01:34 +0300

    added

--- app/components/page.jsx
+++ app/components/page.jsx
@@ -1,3 +1,22 @@
 export default function Page() {
-  return <div></div>;
+  return (
+    <div className=" mx-auto">
+      <h2 className="text-2xl font-bold mb-4">Introduction</h2>
+      <p className="leading-7 text-lg">
+        Discover a collection of beautifully designed, re-usable components
+        crafted for your Next.js projects.
+      </p>
+      <p className="leading-7 text-lg mt-4">
+        This is a collection of components that you can easily copy and paste
+        directly into your projects.
+      </p>
+      <p className="leading-7 text-lg mt-4">
+        Pick the components that suit your needs, customize them to your liking,
+        and make them your own. Use this collection as a reference or
+        inspiration to build your own component libraries, tailored to your
+        specific project requirements. The code is yours to mold and adapt as
+        you see fit.
+      </p>
+    </div>
+  );
 }
