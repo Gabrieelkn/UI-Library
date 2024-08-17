5d22f61bab849cdcff5836bdb3a4344dcf9fabb3
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-08-17 17:59:39 +0300

    added

---
+++ shop/card/card.jsx
@@ -0,0 +1,18 @@
+export default function CardWrapper() {
+  return (
+    <div className="h-96 grid place-items-center w-full">
+      <Card title="Title" text="This is the text for the card" />
+    </div>
+  );
+}
+
+function Card({ title, text }) {
+  return (
+    <div className=" w-3/4 flex items-center justify-center z-50">
+      <div className="bg-gray-800 rounded-lg shadow-lg w-3/4 p-6 relative">
+        <h2 className="text-xl text-white font-semibold mb-4">{title}</h2>
+        <p className="text-white">{text}</p>
+      </div>
+    </div>
+  );
+}
