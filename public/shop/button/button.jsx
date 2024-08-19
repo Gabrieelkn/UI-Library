8d594be30b8e90af313637a53f16bf4e77b2391a
Gabrieelkn <112697117+Gabrieelkn@users.noreply.github.com>
2024-08-19 07:33:14 +0300

    update the styles

--- public/shop/button/button.jsx
+++ public/shop/button/button.jsx
@@ -1,54 +1,16 @@
 "use client";
-// components/Button.js
-import PropTypes from "prop-types";
 
-const Button = ({
-  variant = "primary",
-  size = "medium",
-  disabled = false,
-  onClick = () => {},
-  children,
-}) => {
-  // Base styles for the button
-  const baseStyle =
-    "px-4 py-2  rounded focus:outline-none transition-colors duration-300";
-
-  // Variant styles
-  const variantStyles = {
-    primary: "bg-blue-500 text-white hover:bg-blue-600",
-    secondary: "bg-gray-500 text-white hover:bg-gray-600",
-    outline:
-      "bg-transparent border border-gray-500 text-gray-500 hover:bg-gray-100",
-  };
-
-  // Size styles
-  const sizeStyles = {
-    small: "text-sm py-1 px-2",
-    medium: "text-base py-2 px-4",
-    large: "text-lg py-3 px-6",
-  };
-
-  // Combine base styles, variant styles, size styles, and disabled state
-  const buttonStyles = `${baseStyle} ${variantStyles[variant]} ${
-    sizeStyles[size]
-  } ${disabled ? "opacity-50 cursor-not-allowed" : ""}`;
-
+const Button = ({ children, onClick }) => {
   return (
     <div className="w-full h-96 grid place-items-center">
-      <button className={buttonStyles} onClick={onClick} disabled={disabled}>
+      <button
+        onClick={onClick}
+        className="px-4 py-2 rounded focus:outline-none transition-transform duration-300 transform hover:scale-105 shadow-lg bg-mainColor text-white hover:bg-indigo-600 text-sm py-3 px-5"
+      >
         {children}
       </button>
     </div>
   );
-};
-
-// Define PropTypes to enforce component usage
-Button.propTypes = {
-  variant: PropTypes.oneOf(["primary", "secondary", "outline"]),
-  size: PropTypes.oneOf(["small", "medium", "large"]),
-  disabled: PropTypes.bool,
-  onClick: PropTypes.func,
-  children: PropTypes.node.isRequired,
 };
 
 export default Button;
